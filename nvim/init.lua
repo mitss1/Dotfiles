ad9acfaa90e39075f83c0c3d90c867da2010a1ca
mitss1 <mats.soiland@maksimer.com>
2025-03-11 11:13:58 +0100

    Disable spellchecking for markdown

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,11 @@
 require('user/plugins')
 require('user/options')
 require('user/keymaps')
+
+-- Disable spell checking for markdown files (including Obsidian notes)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "md" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
